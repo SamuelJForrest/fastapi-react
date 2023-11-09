782d959cddadcf22989ba404faaee3be56e69456
Sam Forrest <samforrest@live.co.uk>
2023-11-09 19:59:27 +0000

    IMP: .env file

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,11 @@
+import os
+from dotenv import load_dotenv
 import uvicorn
+
+load_dotenv()
+
+TEST = os.environ.get('TEST')
+print(TEST)
 
 if __name__ == "__main__":
     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
